a7d23b06d030a42b49e874a460f8e1ca967be285
Pangpond <pangpond5344@gmail.com>
2021-10-30 22:45:11 +0700

    homework10

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: const TextTheme(
+        fontFamily: GoogleFonts.prompt().fontFamily,
+        primarySwatch: Colors.purple,
+        textTheme: TextTheme(
           headline1: TextStyle(
             fontSize: 30.0,
             fontWeight: FontWeight.bold,
-            color: Colors.black,
           ),
           headline6: TextStyle(
-            fontSize: 30.0,
-            fontWeight: FontWeight.bold,
+            fontSize: 22.0,
+            //fontWeight: FontWeight.bold,
           ),
-          bodyText2: TextStyle(fontSize: 14.0),
+          bodyText2: TextStyle(
+            fontSize: 14.0,
+          ),
         ),
       ),
-      home: LoginPage2(),
+      //home: LoginPage(),
+      routes: {
+        LoginPage.routeName: (context) => const LoginPage(),
+        HomePage.routeName: (context) => const HomePage(),
+        FoodListPage.routeName: (context) => const FoodListPage(),
+        FoodDetailsPage.routeName: (context) => const FoodDetailsPage(),
+      },
+      initialRoute: LoginPage.routeName,
     );
   }
 }
